de6fe5345501626627663fed77302c1335dd9b0f
Z1P TI3 <z1pti3@outlook.com>
2020-10-06 19:43:11 +0100

    Updated web to use ACL

--- web/event.py
+++ web/event.py
@@ -4,12 +4,13 @@ from flask import current_app as app
 from pathlib import Path
 import time
 
+from core import api
 from plugins.event.models import event
 
 pluginPages = Blueprint('eventPages', __name__, template_folder="templates")
 
 @pluginPages.route("/event/")
 def mainPage():
-    findActiveEvents = event._event().getAsClass(query={"expiryTime" : { "$gt" : time.time() } })
+    findActiveEvents = event._event().getAsClass(sessionData=api.g.sessionData,query={"expiryTime" : { "$gt" : time.time() } })
     return render_template("event.html", events=findActiveEvents)
 
