2bd89c4776799dbbe628c566791984930d01dec0
z1pti3 <z1pti3@outlook.com>
2021-01-17 16:34:09 +0000

    BETA UI for corrilations

--- web/event.py
+++ web/event.py
@@ -14,3 +14,32 @@ def mainPage():
     findActiveEvents = event._event().getAsClass(sessionData=api.g.sessionData,query={"expiryTime" : { "$gt" : time.time() } })
     return render_template("event.html", events=findActiveEvents)
 
+@pluginPages.route("/event/<eventID>/")
+def getEvent(eventID):
+    foundEvent = event._event().query(sessionData=api.g.sessionData,id=eventID)["results"][0]
+    return foundEvent, 200
+
+@pluginPages.route("/event/eventCorrelation/<eventCorrelationID>/")
+def eventCorrelationPage(eventCorrelationID):
+    return render_template("eventCorrelation.html")
+
+
+@pluginPages.route("/event/eventCorrelation/<eventCorrelationID>/get/")
+def getEventCorrelation(eventCorrelationID):
+    eventCorrelation = event._eventCorrelation().getAsClass(sessionData=api.g.sessionData,id=eventCorrelationID)[0]
+    correlationMap = {}
+    for sourceEvent, targetEvent in ((sourceEvent, targetEvent) for sourceEvent in eventCorrelation.events for targetEvent in eventCorrelation.events):
+        for field, fieldValue in sourceEvent["eventValues"].items():
+            try:
+                if fieldValue in targetEvent["eventValues"][field]:
+                    mapKey = "{0}->{1}".format(sourceEvent["_id"],targetEvent["_id"])
+                    if mapKey not in correlationMap:
+                        correlationMap[mapKey] = { "source" : sourceEvent["_id"], "target" : targetEvent["_id"], "matches" : [] }
+                    correlationMap[mapKey]["matches"].append([field,fieldValue])
+            except KeyError:
+                pass
+    return correlationMap, 200
+
+
+
+
